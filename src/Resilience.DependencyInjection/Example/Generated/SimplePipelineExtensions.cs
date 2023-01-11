fde8bcbbaa133f38e65b1e45351852894770d5c5
Martin Tomka <martintomka@microsoft.com>
2023-01-11 14:29:57 +0100

    Generators for custom pipelines demo

---
+++ src/Resilience.DependencyInjection/Example/Generated/SimplePipelineExtensions.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Resilience.Strategies.Retry;
+using Resilience.Strategies.Timeout;
+
+namespace Resilience.DependencyInjection.Example.Generated;
+
+internal static class SimplePipelineExtensions
+{
+    public static IServiceCollection AddSimplePipeline(this IServiceCollection services, Action<SimplePipelineOptions>? configure = null, IConfigurationSection? section = null)
+    {
+        var builder = services.AddOptions<SimplePipelineOptions>("SimplePipeline");
+
+        if (configure != null)
+        {
+            builder.Configure(configure);
+        }
+
+        if (section != null)
+        {
+            builder.Bind(section);
+        }
+
+        services.AddResilienceStrategy("SimplePipeline", (serviceProvider, builder) =>
+        {
+            var options = serviceProvider.GetRequiredService<IOptionsMonitor<SimplePipelineOptions>>().Get("SimplePipeline");
+
+            builder.AddTimeout(options.TotalTimeout);
+            builder.AddRetry(options.Retry);
+            builder.AddTimeout(options.AttemptTimeout);
+        });
+
+        return services;
+    }
+}
---
+++ src/Resilience.DependencyInjection/Example/SimplePipelineOptions.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Resilience.Strategies.Retry;
+using Resilience.Strategies.Timeout;
+
+namespace Resilience.DependencyInjection.Example;
+
+[ResilienceStrategy]
+public class SimplePipelineOptions
+{
+    public TimeoutStrategyOptions TotalTimeout { get; set; } = new();
+
+    public RetryStrategyOptions Retry { get; set; } = new();
+
+    public TimeoutStrategyOptions AttemptTimeout { get; set; } = new();
+}
